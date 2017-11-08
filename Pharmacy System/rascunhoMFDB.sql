8c6d21b93760309503c85513eaab6325195bac9a
Eulle Silva Araújo <eulle@imd.local>
2017-11-08 19:37:30 -0300

    Foi adicionada novas tabelas

--- Pharmacy System/rascunhoMFDB.sql
+++ Pharmacy System/rascunhoMFDB.sql
@@ -14,7 +14,7 @@ CREATE TABLE Cliente (
 	nomeCliente VARCHAR(45) NOT NULL,
 	cpf INTEGER NOT NULL UNIQUE,
 	telefone INTEGER ,
-	endereço VARCHAR(45) NOT NULL,
+	endereco VARCHAR(45) NOT NULL,
 	sexo VARCHAR(10) NOT NULL CHECK(sexo = 'M' or sexo = 'F' or sexo = 'Outro'),
 	dataNascimento 	VARCHAR(10) NOT NULL,
 
@@ -31,14 +31,14 @@ CREATE TABLE Login (
 );
 
 
-CREATE TABLE Funcionário (
+CREATE TABLE Funcionario (
 
 	idFuncionario INTEGER NOT NULL,
 	nomeFuncionario VARCHAR(45) NOT NULL,
 	cpf INTEGER NOT NULL UNIQUE,
-	cargo VARCHAR(30) NOT NULL CHECK (cargo = 'Farmacêutico' or cargo = 'Vendedora'), --- Mais coisas
+	cargo VARCHAR(30) NOT NULL CHECK (cargo = 'Farmacêutico' or cargo = 'Balconista'), --- Mais coisas
 	telefone INTEGER ,
-	endereço VARCHAR(45) NOT NULL,
+	endereco VARCHAR(45) NOT NULL,
 	sexo VARCHAR(10) NOT NULL CHECK(sexo = 'M' or sexo = 'F' or sexo = 'Outro'),
 	dataNascimento VARCHAR(10) NOT NULL,
 
@@ -64,8 +64,8 @@ CREATE TABLE Produto (
 	idProduto INTEGER NOT NULL,
 	nomeProduto VARCHAR(45) NOT NULL,
 	tipoProduto VARCHAR(30) NOT NULL,
-	preço FLOAT NOT NULL,
-	dataFabricação VARCHAR(10) NOT NULL,
+	preco FLOAT NOT NULL,
+	dataFabricacao VARCHAR(10) NOT NULL,
 	dataValidade VARCHAR(10) NOT NULL,
 	lote INTEGER NOT NULL,
 	
@@ -75,7 +75,72 @@ CREATE TABLE Produto (
 
 CREATE TABLE Estoque (
 	idEstoque INTEGER NOT NULL,
+	idProduto INTEGER NOT NULL REFERENCES Produto,
 	quantidade INTEGER ,
-	localização VARCHAR(30) NOT NULL,
+	localizacao VARCHAR(30) NOT NULL,
 	
+	PRIMARY KEY(idEstoque)
+
 );
+
+
+CREATE TABLE Devolucao (
+	idDevelocao INTEGER NOT NULL,
+	idVenda INTEGER NOT NULL REFERENCES Venda,
+	idProduto INTEGER NOT NULL REFERENCES Produto,
+	motivo VARCHAR(40) NOT NULL,
+
+	PRIMARY KEY(idDevelocao)
+)
+
+
+CREATE TABLE Venda(
+	idVenda INTEGER NOT NULL,
+	idFuncinario INTEGER NOT NULL REFERENCES Funcionario,
+	dataVenda VARCHAR(10) NOT NULL,
+	formaPagamento VARCHAR(10) NOT NULL CHECK (formaPagamento = 'Dinheiro' or formaPagamento = 'Cartão'), 
+	valorTotal FLOAT NOT NULL,
+
+	PRIMARY KEY(idVenda)
+)
+
+CREATE TABLE Pedido(
+	idPedido INTEGER NOT NULL,
+	idProduto INTEGER NOT NULL REFERENCES Produto,
+	idCliente INTEGER NOT NULL REFERENCES Cliente,
+	idVenda INTEGER NOT NULL REFERENCES Venda,
+	quantidade INTEGER NOT NULL,
+	dataPedido VARCHAR(10) NOT NULL,
+
+	PRIMARY KEY(idPedido)
+)
+
+CREATE TABLE Receita(
+	idReceita INTEGER NOT NULL,
+	dataPrescricao VARCHAR(10) NOT NULL,
+	crm INTEGER NOT NULL REFERENCES Medico,
+
+	PRIMARY KEY(idReceita)
+)
+
+CREATE TABLE Medico(
+	crm INTEGER NOT NULL,
+	nomeMedico VARCHAR(40) NOT NULL,
+
+	PRIMARY KEY(crm)
+)
+
+CREATE TABLE ReceitaRemedio(
+	idReceita INTEGER NOT NULL,
+	idRemedio INTEGER NOT NULL,
+
+	PRIMARY KEY(idReceita),
+	PRIMARY KEY(idRemedio)
+)
+
+CREATE TABLE Remedio(
+	idRemedio INTEGER NOT NULL,
+	nomeRemedio VARCHAR(40) NOT NULL,
+
+	PRIMARY KEY(idRemedio)
+)
