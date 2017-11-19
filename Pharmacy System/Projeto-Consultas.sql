93396c8a70a2fbac46f4dc9b2b953b9bbd978102
judsonmatheus <judson.matheus.andrade@gmail.com>
2017-11-19 11:43:53 -0200

    Adicionando consultas
    As consultas fazem para do relatório descrita no nosso trabalho escrito

---
+++ Pharmacy System/Projeto-Consultas.sql
@@ -0,0 +1,29 @@
+
+--Busca o numero de funcionario pelos diferentes sexos
+select sexo, count(sexo) 
+from funcionario 
+group by sexo
+
+--Busca o numero de funcionario pelos diferentes cargos
+select cargo, count(cargo)
+from funcionario
+group by cargo 
+
+--Busca o numero de clientes pelos diferentes sexos
+select sexo, count(sexo)
+from cliente
+group by sexo
+
+--Agrupa os clientes pela idade
+select date_trunc('year',age(current_date,dataNascimento)), count(date_trunc('year',age(current_date,dataNascimento)))
+from cliente
+group by date_trunc('year',age(current_date,dataNascimento))
+
+--Nome dos medico que venderam algum remedio
+select nomemedico from pedido inner join receita 
+on pedido.idReceita = receita.idReceita
+inner join medico on receita.crm = medico.crm
+where pedido.quantidade > 0
+
+
+
